df40ebae79b2cf7ed34b9b6ce86bb64d8cdff242
Gustavo Paschoalin <pschln@iMac-de-Gustavo.local>
2022-07-23 12:52:49 -0300

    adding source files

---
+++ src/common/utils/time.ts
@@ -0,0 +1,7 @@
+export function tempoParaSegundos(tempo: string) {
+    const [horas = '0', minutos = '0', segundos = '0'] = tempo.split (":");
+
+    const horasEmSegundos = Number(horas) * 3600;
+    const minutosEmSegunto = Number(minutos) * 60;
+    return horasEmSegundos + minutosEmSegunto + Number(segundos);
+}
